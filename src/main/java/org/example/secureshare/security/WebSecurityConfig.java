b4788790ba5f11eedc9e2758994ac9b0750d6b2c
Jagdish Everest <119604053+jagdisheverest2004@users.noreply.github.com>
2025-09-02 13:08:47 +0530

    init commit

--- src/main/java/org/example/secureshare/security/WebSecurityConfig.java
+++ src/main/java/org/example/secureshare/security/WebSecurityConfig.java
@@ -6,8 +6,10 @@ import org.example.secureshare.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,6 +21,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -55,16 +59,18 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                                 .requestMatchers("/api/auth/authenticate/signup").permitAll()
                                 .requestMatchers("/api/auth/authenticate/signin").permitAll()
                                 .requestMatchers("/api/auth/authenticate/verify-otp").permitAll()
                                 .requestMatchers("/api/auth/authenticate/signout").authenticated()
-                                .requestMatchers("/api/auth/otp/").permitAll()
+                                .requestMatchers("/api/auth/otp/**").permitAll()
                                 .requestMatchers("/api/auth/user-utils/initiate").permitAll()
                                 .requestMatchers("/api/auth/user-utils/reset").permitAll()
                                 .requestMatchers("/api/auth/user-utils/find-username").permitAll()
